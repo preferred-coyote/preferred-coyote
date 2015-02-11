a7b9c07c6250b90cbd8536a479375c1d7a1d9644
Jackson Hoose <jacksonhoose@gmail.com>
2015-02-10 23:53:58 -0800

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var karma = require('karma').server;
+var watch = require('gulp-watch');
+var runSequence = require('run-sequence');
+
+var paths = {
+  src: {
+    bower: './client/src/bower_components',
+    img: './client/src/img',
+    scss: './client/src/scss'
+  },
+  dist: {
+    css: './client/dist/css',
+    img: './client/dist/img'
+  },
+  karmaConf: __dirname + '/spec/karam.conf.js'
+};
+
+var handleError = function(err){
+  console.log(err.toString());
+  this.emit('end');
+};
+
+gulp.task('lint', function(){
+  // lint files
+});
+
+gulp.task('test', function(){
+  // runSequence('karma');
+});
+
+gulp.task('karma', function(done){
+  return karma.start({
+    configFile: paths.karmaConf,
+    singleRun: true
+  }, done);
+});
