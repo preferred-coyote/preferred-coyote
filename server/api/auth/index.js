ea93812a4e819080ca589ee0c579d5adc4f085a0
Yan Fan <theyanfan@gmail.com>
2015-02-12 19:27:45 -0800

    Add auth folder + files

---
+++ server/api/auth/index.js
@@ -0,0 +1,18 @@
+var router = require('express').Router();
+var authController = require('./authController');
+
+module.exports = function(app) {
+
+  // login
+  router.post('/login', authController.login);
+  // signin
+  router.post('/signin', authController.signin);
+  // logout
+  router.get('/logout', authController.logout);
+
+
+
+  // mount router onto /api
+  app.use('/api', router);
+
+};
