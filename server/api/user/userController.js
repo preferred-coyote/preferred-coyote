d9b20ed0b1c2c583739ee16b549cac56a9b61669
Jackson Hoose <jacksonhoose@gmail.com>
2015-02-12 17:18:15 -0800

    add some user routes

--- server/api/user/userController.js
+++ server/api/user/userController.js
@@ -1,6 +1,29 @@
 module.exports.list = function(req, res, next) {
-  res.json({
+  res.json([{
     id: 1,
     name: 'Bob'
+  }, {
+    id:2,
+    name: 'Hank'
+  }]);
+};
+
+module.exports.create = function(req, res, next) {
+  res.status(201).json({
+    message: 'User Created'
+  });
+};
+
+module.exports.show = function(req, res, next) {
+  var id = req.params.id;
+  res.status(200).json({
+    id: id,
+    user: 'Bob'
+  });
+};
+
+module.exports.update = function(req, res, next) {
+  res.json({
+    message: 'update a user'
   });
 };
