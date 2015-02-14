7afc04a4d36caf76c1eaf40724108a5da1ea816c
Yan Fan <theyanfan@gmail.com>
2015-02-13 19:34:54 -0800

    Delete Q dependency and fix auth/login formatting

--- app/components/auth/login.js
+++ app/components/auth/login.js
@@ -7,9 +7,9 @@ var Login = React.createClass({
   render: function() {
     return ( 
       <form className="form" role="form" action="/api/auth/login" method="POST">
-      <input type="text" name="username" placeholder="username" />
-      <input type="password" name="password" />
-      <button type="submit"> Login </button>
+	      <input type="text" name="username" placeholder="username" />
+	      <input type="password" name="password" />
+	      <button type="submit"> Login </button>
       </form>
     );
   }
--- app/components/auth/signup.js
+++ app/components/auth/signup.js
@@ -5,10 +5,12 @@ var Button = require('../ui/button').Button;
 
 var Login = React.createClass({
   render: function() {
-    return ( <form className="form" role="form" action="/api/auth/login" method="POST">
-      <input type="text" name="username" placeholder="username"/>
-      <input type="password" name="password"/>
-      <button type="submit"> Login </button> </form>
+    return ( 
+    	<form className="form" role="form" action="/api/auth/login" method="POST">
+	      <input type="text" name="username" placeholder="username"/>
+	      <input type="password" name="password"/>
+	      <button type="submit"> Login </button>
+      </form>
     );
   }
 });
