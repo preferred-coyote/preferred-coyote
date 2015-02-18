41313d507c4d49f57fc7013ce7f262da6c5d56cc
Jackson Hoose <jacksonhoose@gmail.com>
2015-02-18 15:45:37 -0800

    user api

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,8 @@ module.exports = function(sequelize, DataTypes) {
 
   var User = sequelize.define('User', {
     username: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    profile: DataTypes.TEXT
   }, {
     instanceMethods: {
       comparePasswords: function(candidatePassword) {
