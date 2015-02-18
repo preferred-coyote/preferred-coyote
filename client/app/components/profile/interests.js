09a557ffbaccd5ef0147f73896632d482d8fb66f
Alexander Tseung <alextsg@gmail.com>
2015-02-17 19:48:09 -0800

    modified profiles styles

---
+++ client/app/components/profile/interests.js
@@ -0,0 +1,22 @@
+var React = require('react');
+
+var Interests = React.createClass({
+  getInitialState: function() {
+    return {
+      data: this.props.interests.map(function(element) {
+        return (<div>{element.name}</div>);
+      })
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h3>Interests</h3>
+        {this.state.data}
+      </div>
+    );
+  }
+});
+
+module.exports.Interests = Interests;
